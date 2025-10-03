a4977ffa2b6d3ab5bbca8daad64622bb616b72ce
Your Name <you@example.com>
2025-10-03 11:01:43 -0400

    Prep for GitHub Actions build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    // Corrected Android Gradle Plugin version from 8.13.0 to a valid one
+    id("com.android.application") version "8.2.0" apply false
+
+    // Updated Kotlin plugin to a more recent, stable version
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
